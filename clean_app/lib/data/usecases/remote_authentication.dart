05061d58f77a312367ed1aed1cbaab26b46c831c
raphaelbarbosaqwerty <raphaelcorreabarbosa@hotmail.com>
2021-01-19 22:04:27 -0300

    make RemoteAuthentication implements Authentication

--- clean_app/lib/data/usecases/remote_authentication.dart
+++ clean_app/lib/data/usecases/remote_authentication.dart
@@ -6,7 +6,7 @@ import 'package:meta/meta.dart';
 import '../../domain/usecases/usecases.dart';
 import '../http/http.dart';
 
-class RemoteAuthentication {
+class RemoteAuthentication implements Authentication {
   final HttpClient httpClient;
   final String url;
 
