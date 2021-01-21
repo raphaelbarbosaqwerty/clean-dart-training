aa62d95d42c64329dc89e08921e93cab4fc5e3a1
raphaelbarbosaqwerty <raphaelcorreabarbosa@hotmail.com>
2021-01-20 21:03:01 -0300

    enseure HttpAdapter throws ServerError if post throws

--- clean_app/test/infra/http/http_adapter_test.dart
+++ clean_app/test/infra/http/http_adapter_test.dart
@@ -34,6 +34,12 @@ void main() {
     void mockResponse(int statusCode, {String body = '{"any_key":"any_value"}'}) {
       mockRequest().thenAnswer((realInvocation) async => Response(body, statusCode));
     }
+
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
+
     setUp(() {
       mockResponse(200);
     });
@@ -118,6 +124,12 @@ void main() {
 
     test('Should return ServerError if post returns 500', () async {
       mockResponse(500);
+      final future = sut.request(url: url, method: 'post');
+      expect(future, throwsA(HttpError.serverError));
+    });
+
+    test('Should return ServerError if post throws', () async {
+      mockError();
       final future = sut.request(url: url, method: 'post');
       expect(future, throwsA(HttpError.serverError));
     });
